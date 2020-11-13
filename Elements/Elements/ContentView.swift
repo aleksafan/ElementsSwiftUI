f2e8721920fa4bd209e5c47fd9c6590d59a4d341
aleksafan <a.n.afanasieva@ya.ru>
2020-11-12 21:12:59 -0500

    1

---
+++ Elements/Elements/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Elements
+//
+//  Created by Aleksandra Afanasieva on 2020-11-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Elements/Elements/ElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElementsApp.swift
+//  Elements
+//
+//  Created by Aleksandra Afanasieva on 2020-11-12.
+//
+
+import SwiftUI
+
+@main
+struct ElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
