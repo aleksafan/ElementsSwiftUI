18e7e22a236f4be11f6e6f4da69c1431874c10e5
aleksafan <a.n.afanasieva@ya.ru>
2020-11-12 21:16:25 -0500

    New view

---
+++ Elements/Elements/SignUpFirebaseView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpFirebase.swift
+//  Elements
+//
+//  Created by Aleksandra Afanasieva on 2020-11-12.
+//
+
+import SwiftUI
+
+struct SignUpFirebase: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpFirebase_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpFirebase()
+    }
+}
